46728e5be27e4a6e6d6119771d6c925a23cddeb1
dlsutton2 <69909157+dlsutton2@users.noreply.github.com>
2022-03-22 17:29:46 -0600

    Add files via upload

---
+++ util/util.php
@@ -0,0 +1,15 @@
+<?php
+    /**
+     * @param {Array[]} $arr - Associative array of key, value pairs
+     * @return {String[]} - Array of equal comparison expressions, e.g. ["make_id = 1", "type_id = 2"]
+     */
+    function get_query_expressions($arr) {
+        $query_array = array();
+        foreach($arr as $key => $value) {
+            if ($value != '') {
+                $query_array[] = $key . ' = ' . $value;
+            }
+        }
+        return $query_array;
+    }
+?>
